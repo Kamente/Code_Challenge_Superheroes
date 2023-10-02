0328982c35ad31133caa8c3399a8c4eeb8ea8913
Kamente <justinm48000@gmail.com>
2023-10-02 20:43:03 +0300

    rectify the relationship error

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -9,7 +10,15 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     super_name = db.Column(db.String(255), nullable=False)
-    powers = db.relationship('HeroPower', backref='hero', lazy=True)
+    heropowers = db.relationship('HeroPower', backref='hero', lazy=True)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'super_name': self.super_name,
+            'powers': [power.as_dict() for power in self.powers]
+        }
 
 
 class Power(db.Model):
@@ -18,7 +27,14 @@ class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
-    heroes = db.relationship('HeroPower', backref='power', lazy=True)
+    heropowers = db.relationship('HeroPower', backref='power', lazy=True)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
 
 
 class HeroPower(db.Model):
@@ -28,13 +44,18 @@ class HeroPower(db.Model):
     strength = db.Column(db.String(255), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey("power.id"), nullable=False)
     hero_id = db.Column(db.Integer, db.ForeignKey("hero.id"), nullable=False)
-    hero = db.relationship('Hero', backref='powers')
-    power = db.relationship('Power', backref='heroes')
 
     VALID_STRENGTHS = {'Strong', 'Weak', 'Average'}
 
     @validates('strength')
     def validate_strength(self, key, value):
         if value not in self.VALID_STRENGTHS:
-            raise ValueError("Invalid strength value")
+            raise ValueError("Invalid strength values")
         return value
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'strength': self.strength,
+            'power': self.power.as_dict()
+        }
