b90a809b134bbe69be281ebccce07e33db0abd78
Kamente <justinm48000@gmail.com>
2023-10-04 04:28:25 +0300

    GET request methods for Heroes

--- app/app.py
+++ app/app.py
@@ -2,10 +2,11 @@
 
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
+# dburi = 'postgres://paxful_login_user:vWseP9sNrGRMsqCnDyiMErVC5OcP5LLg@dpg-ckceqh6smu8c73cb0eng-a.oregon-postgres.render.com/paxful_login'
+# app.config['SQLALCHEMY_DATABASE_URI'] = configx(dburi)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -22,24 +23,6 @@ def home():
     return {
         'message': 'Welcome to SuperHeroes'
     }, 200
-
-
-@app.route('/heroes', methods=['GET'])
-def get_heroes():
-    heroes_list = Hero.query.all()
-    if heroes_list:
-        return jsonify([hero.as_dict() for hero in heroes_list]), 200
-    else:
-        return jsonify({'error': 'Hero not found'}), 404
-
-
-@app.route('/heroes/<int:id>', methods=['GET'])
-def get_hero_by_id(id):
-    hero = Hero.query.get(id)
-    if hero:
-        return jsonify(hero.as_dict()), 200
-    else:
-        return jsonify({'error': 'Hero not found'}), 404
 
 
 @app.route('/powers', methods=['GET'])
@@ -74,6 +57,30 @@ def update_power(id):
     return jsonify(power.as_dict()), 200
 
 
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes_list = Hero.query.all()
+    if heroes_list:
+        return jsonify([{'id': her.id, 'name': her.name, 'super_name': her.super_name} for her in heroes_list]), 200
+    else:
+        return jsonify({'error': 'No Hero found'})
+
+
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero_by_id(id):
+    hero = Hero.query.get(id)
+    if hero:
+        power_data = {
+            'id': hero.id,
+            'name': hero.name,
+            'super_name': hero.super_name,
+            'powers': [{'id': hp.power.id, 'name': hp.power.name, 'description': hp.power.description} for hp in hero.heropowers]
+        }
+        return jsonify(power_data), 200
+    else:
+        return jsonify({'error': 'Hero Not Found'}), 404
+
+
 @app.route('/hero_powers', methods=['POST'])
 def create_hero_power():
     data = request.get_json()
@@ -81,14 +88,17 @@ def create_hero_power():
     hero_id = data.get('hero_id')
     power_id = data.get('power_id')
     strength = data.get('strength')
-
+    
+    if not hero_id or not power_id or not strength:
+        return jsonify({'errors': ["Validation Errors"]})
+    
     hero = Hero.query.get(hero_id)
     power = Power.query.get(power_id)
 
     if not hero or not power:
         return jsonify({'error': 'Hero or Power not found'}), 404
 
-    hero_power = HeroPower(hero=hero, power=power, strength=strength)
+    hero_power = HeroPower(hero_id=hero, power_id=power, strength=strength)
     db.session.add(hero_power)
     db.session.commit()
 
